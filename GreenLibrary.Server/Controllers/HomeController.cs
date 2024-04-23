5bab61799154e07199a94130df2714d61b704a7a
Kremena Nikolova <aradia1992@gmail.com>
2024-04-23 17:25:43 +0300

    create article service and take all articles in the controller

---
+++ GreenLibrary.Server/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿namespace GreenLibrary.Server.Controllers
+{
+    using GreenLibrary.Services.Interfaces;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IArticleService articleService;
+
+        public HomeController(IArticleService articleService)
+        {
+            this.articleService = articleService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var allArticles = await articleService.GetAllArticlesAsync();
+
+            return Ok(allArticles);
+        }
+    }
+}
